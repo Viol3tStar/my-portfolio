989d6cb39ec0ac13b66a7908ddc1eeef2d107c13
Viol3tStar <vcming95z@gmail.com>
2024-05-02 03:15:02 +0800

    update const title and desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My Portfolio";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Ng Wai Ming";
+export const SITE_DESCRIPTION =
+  "Ng Wai Ming is a front-end developer who specialize in crafting user-friendly digital experiences that are visually captivating and inclusive.";
